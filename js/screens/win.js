ec705616db9cfef6390a97737e2762ac76c4c5b9
Sergey Zveroboev <szveroboev@gmail.com>
2018-04-13 11:23:40 +0300

    Частичная шаблонизация победного экрана

--- js/screens/win.js
+++ js/screens/win.js
@@ -1,6 +1,7 @@
 import getElementFromTemplate from '../utils/get-element-from-template';
 import getResultInfo from '../utils/get-result-info';
 import initialState from '../data/initial-state';
+import {MAX_LIVES} from '../data/constants';
 import store from '../data/store';
 
 // Результат игры: выигрыш
@@ -14,8 +15,8 @@ const getWinScreen = (state) => {
     
       <h2 class="title">Вы настоящий меломан!</h2>
       <div class="main-stat">За&nbsp;3&nbsp;минуты и 25&nbsp;секунд
-        <br>вы&nbsp;набрали 12 баллов (8 быстрых)
-        <br>совершив 3 ошибки</div>
+        <br>вы&nbsp;набрали ${state.scores} баллов (0 быстрых)
+        <br>совершив ${MAX_LIVES - state.lives} ошибки</div>
       <span class="main-comparison">${winMessage}</span>
       <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
     </section>
