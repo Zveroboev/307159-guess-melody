dfc2a8e1afdf961fb75c679282f5c4088d585487
Sergey Zveroboev <szveroboev@gmail.com>
2018-04-13 11:28:28 +0300

    Починил тревиса

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import renderNextScreen from './utils/render-next-screen';
 import store from './data/store';
 
-store.subscribe(() => console.log(`--- new State: `, store.getState()));
+// store.subscribe(() => console.log(`--- new State: `, store.getState()));
 store.subscribe(renderNextScreen);
 
 renderNextScreen();
