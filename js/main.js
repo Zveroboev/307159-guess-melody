ce4b65017ea2f1d51136290f5273afd5cd98caea
Sergey Zveroboev <szveroboev@gmail.com>
2018-05-03 16:20:17 +0300

    change gulpfile.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+import 'babel-polyfill';
+import 'whatwg-fetch';
 import Application from './application';
 
 Application.start();
+
+// npm i -DE rollup-plugin-babel babel-plugin-external-helpers babel-preset-env rollup-plugin-node-resolve rollup-plugin-commonjs
